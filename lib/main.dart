61d74395077ed867d003e0d883f931734766ba2f
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2021-04-18 13:49:21 +0530

    debug baaner removed

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ProviderScope(
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
